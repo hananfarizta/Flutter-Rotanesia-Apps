eca6cc8ee0f62b3b5f5fbfe42e9fa816b7de61a2
Muhammad Hanan Rafif Farizta <hananfariztaa@gmail.com>
2022-07-02 05:43:58 +0700

    add theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 25.0;
+
+Color whiteColor = const Color(0xFFFFFFFF);
+Color redColor1 = const Color(0xFFC32334);
+Color redColor2 = const Color(0xFFEC8177);
+Color redColor3 = const Color(0xFFFBDDD2);
+Color redColor4 = const Color(0xFFFE3A30);
+Color blackColor1 = const Color(0xFF444444);
+Color blackColor2 = const Color(0xFF1C1C1C);
+Color grayColor1 = const Color(0xFF8E8D8D);
+Color grayColor2 = const Color(0xFFC4C4C4);
+Color grayColor3 = const Color(0xFFE1E1E1);
+Color grayColor4 = const Color(0xFF828282);
+Color grayColor5 = const Color(0xFFEBEBEB);
+Color blueColor = const Color(0xFF0E70E3);
+Color greenColor1 = const Color(0xFF019557);
+Color greenColor2 = const Color(0xFF0ACF83);
+Color walletColor = const Color(0xFFF96464);
+Color hatColor = const Color(0xFFF0C398);
+Color bagColor = const Color(0xFFFF6E56);
+Color additionColor = const Color(0xFFE3C84C);
+Color fittingsColor = const Color(0xFFFF936B);
+Color footwearColor = const Color(0xFFEBB58F);
+Color goldColor = const Color(0xFFBD7D01);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: blackColor1);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: grayColor1);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle redTextStyle1 = GoogleFonts.poppins(color: redColor1);
+TextStyle redTextStyle2 = GoogleFonts.poppins(color: redColor2);
+TextStyle grayTextStyle = GoogleFonts.poppins(color: grayColor2);
+TextStyle goldTextStyle = GoogleFonts.poppins(color: goldColor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: greenColor1);
+
+FontWeight light = FontWeight.w300;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
